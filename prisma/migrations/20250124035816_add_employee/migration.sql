df448b4603a0900a2b141ccd71f086020fe39019
kaungthantzindev <mr.kaungthantzin@gmail.com>
2025-01-24 10:35:31 +0630

    feat : swagger setup

---
+++ prisma/migrations/20250124035816_add_employee/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Test` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Test";
+
+-- CreateTable
+CREATE TABLE "employees" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "employees_pkey" PRIMARY KEY ("id")
+);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('code test api')
+    .setDescription('code test api docunmentation')
+    .build()
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig)
+
+  SwaggerModule.setup('apidoc', app, swaggerDoc)
+
+  await app.listen(process.env.PORT ?? 3000)
+  console.log(`server started at ${await app.getUrl()}`)
 }
-bootstrap();
+bootstrap()
